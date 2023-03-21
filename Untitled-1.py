d67aac27fcad04c10230ada4592d306b0c2330ca
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-03-21 04:46:05 -0300

    Untitled-1.py

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-angulo = float(input('Digite um ângulo em graus: '))
-anulo = math.radians(angulo)
-
-seno = math.sin(angulo)
-cosseno = math.cos(angulo)
-tangente = math.tan(angulo)
-
