b91853e68446ea3e2bb28bd4108f3e13750b95a2
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-03-21 04:47:13 -0300

    Untitled-9.py

--- Untitled-9.py
+++
@@ -1,9 +0,0 @@
-#Exemlo 7 - Aula 04
-
-import os
-os.system('cls' or 'clear')
-
-num = int(input('Digite um numero: '))
-
-resposta = "par" if num%2 == 0 else 'impar'
-print(f'O numero {num} é {resposta}')
