36539ae56a3ed8237506c8a5fbd7824f37f3784a
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-03-20 21:19:17 -0300

    Update Untitled-11.py

--- Untitled-11.py
+++ Untitled-11.py
@@ -1,7 +1,7 @@
 import os
 os.system('cls' or 'clear')
 
-num = float(input('Digite um numero de 0 a 9: '))
+num = float(input('Digite um numero entre 0 e 9: '))
 
-resposta = "Parabens" if num<=9 else 'Errado'
+resposta = "Parabens" if num>=0 and num<=9 else 'Errado'
 print(f'O numero digitado é {num}, {resposta}')
